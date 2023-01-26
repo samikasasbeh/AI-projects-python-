f8193f64ce6ba2e91cc876e4b73416d9f58a25d5
Sami Kasasbeh <Sami.Kasasbehdev@gmail.com>
2023-01-26 04:17:33 -0330

    initiating a classifer and implimenting our first image

--- Facedetection/Face-detector.py
+++ Facedetection/Face-detector.py
@@ -1,8 +1,10 @@
 #import the lib cv2 for our project
 import cv2
-
-
-
-
+#classifer will  help usdetect an existance of a face
+#load some pre-trained data on face frontals from opencv
+trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+ #choose an image to detect faces in. This is how we import an image in open CV
+img = cv2.imread()
+#stopped at 21:59 https://www.youtube.com/watch?v=XIrOM9oP3pA&t=182s
 
 print ("code test")
